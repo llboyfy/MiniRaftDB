e7073b0b4f5740871d1a8c00447fbfafb1e6fbf7
fy <569496465@qq.com>
2025-05-18 13:53:47 +0800

    Initial commit

---
+++ MiniRaftDB/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MiniRaftDB
+//
+//  Created by haolin li on 2025/5/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
